80ea8d62c14adfeb54aa362d0cc22ac450067c2e
gaetos <1786304306@qq.com>
2024-12-04 18:49:50 +0800

    数据库结构修改V2

--- DB/Users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,  -- 用户的唯一ID，自增
-    username VARCHAR(255) NOT NULL,          -- 用户名，不允许为空
-    password VARCHAR(255) NOT NULL,          -- 密码，不允许为空
-    role ENUM('farm', 'factory', 'consumer') NOT NULL,  -- 用户角色，农场(farm)、加工厂(factory)、消费者(consumer)，不允许为空
-    email VARCHAR(255),                      -- 用户的电子邮箱，可以为空
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP  -- 账户创建时间，默认当前时间
-);
